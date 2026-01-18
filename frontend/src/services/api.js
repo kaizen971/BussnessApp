2e6553669d3b7b2f5e0dd73eed7d33a7f6ae8783
Jordan YERBE <87061855+kaizen971@users.noreply.github.com>
2026-01-18 23:03:30 +0100

    fix commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = 'https://mabouya.servegame.com/BussnessApp/BussnessApp';
+const API_BASE_URL = 'https://mabouya.servegame.com/BussnessApp/BussnessApp/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
